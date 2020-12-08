0dc906dd92940a08ce19532024c2a06a603b7774
LON DUFF <johncduffman@gmail.com>
2020-12-07 23:37:50 -0500

    added init information

---
+++ superstore.py
@@ -0,0 +1,21 @@
+class House: 
+    has_basement = False
+    has_garage = False 
+
+    def __init__ (self, squarefootage:int, flooring)
+    self.squarefootage = squarefootage 
+    self.flooring = flooring
+
+class Bedroom(House): 
+    has_bed = False 
+    has_dresser = False
+
+    def __init__ (self, 
+
+class LivingRoom(House): 
+    has_couch = False 
+    has_fireplace = False
+
+    def __init__ (self, )
+
+class Bedroom(House)
